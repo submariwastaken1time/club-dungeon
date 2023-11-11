8fe6900b8b9e6f1d7c38a5f7d62749b6d567fb69
submariwastaken1time <79896755+submariwastaken1time@users.noreply.github.com>
2023-11-11 21:02:59 +0700

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
-
-
 #include "main.hpp"
+#include "SDL2/SDL.h"
 
 Engine engine(80,50);
+SDL_Event event;
 
 int main() {
-    while ( !TCODConsole::isWindowClosed() ) {
-    	engine.update();
-    	engine.render();
-		TCODConsole::flush();
-    }
+  bool quit = false;
+  while (quit = false) {
+      while(SDL_PollEvent(&event) >= 0){
+     if(event.type == SDL_QUIT){
+     quit = true;
+     }
+    engine.update();
+    engine.render();
+		TCOD_console_flush();
+   }
+  }
     return 0;
 }
