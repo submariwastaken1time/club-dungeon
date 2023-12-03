864518d8a8bd7148461fd3417bc73114450cc7ac
submariwastaken1time <79896755+submariwastaken1time@users.noreply.github.com>
2023-12-03 11:51:07 +0700

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,24 @@
 #include "main.hpp"
+
 #include "SDL.h"
 extern "C"
 
-Engine engine(80,50);
+    Engine engine(80, 50);
 bool g_quit = false;
 
-static int QuitFunction(void *userdata, SDL_Event* event) {
- if( event->type == SDL_QUIT){
-   g_quit = true;
- }
- return 0;
+static int QuitFunction(void* userdata, SDL_Event* event) {
+  if (event->type == SDL_QUIT) {
+    g_quit = true;
+  }
+  return 0;
 }
 
-int main(int argc, char *args[]) {
-  SDL_AddEventWatch( QuitFunction , NULL);
+int main(int argc, char* args[]) {
+  SDL_AddEventWatch(QuitFunction, NULL);
   while (g_quit == false) {
-   engine.update();
-   engine.render();
-	 TCODConsole::flush();
- }
- return 0;
+    engine.update();
+    engine.render();
+    TCODConsole::flush();
+  }
+  return 0;
 }
