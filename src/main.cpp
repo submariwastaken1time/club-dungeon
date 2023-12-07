fb7f7c3232aeb79660dc0c5d98bbc7132607e175
submariwastaken1time <79896755+submariwastaken1time@users.noreply.github.com>
2023-12-07 21:11:45 +0700

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include "main.hpp"
-
 #include "SDL.h"
 
 extern "C"
 
-    Engine engine(80, 50);
+Engine engine(80, 50);
+
 bool g_quit = false;
 
 static int QuitFunction(void* userdata, SDL_Event* event) {
@@ -17,12 +17,18 @@ static int QuitFunction(void* userdata, SDL_Event* event) {
 int main(int argc, char* args[]) {
   SDL_AddEventWatch(QuitFunction, NULL);
 
+  SDL_Event player_input;
+
   while (g_quit == false) {
-    while (SDL_PollEvent()) {
-      const SDL_Event sdl_event;
+    while (SDL_PollEvent(&player_input)) {
+      const SDL_Event player_input_processing = player_input;
       TCOD_mouse_t mouse_event;
       TCOD_key_t key_event;
-      tcod::sdl2::process_event(sdl_event, mouse_event) tcod::sdl2::process_event(sdl_event, key_event);
+      TCOD_mouse_t &mouse_event_processed = mouse_event;
+      TCOD_key_t &key_event_processed = key_event;
+      tcod::sdl2::process_event(player_input_processing, mouse_event_processed);
+      tcod::sdl2::process_event(player_input_processing, key_event_processed);
+
     }
     engine.update();
     engine.render();
