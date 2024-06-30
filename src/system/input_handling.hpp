2baae22de75d4fbac91b19aaaa892ed7fa6f63d9
submariwastaken1time <79896755+submariwastaken1time@users.noreply.github.com>
2024-06-30 21:36:18 +0700

    fixed bug where y value wont change

--- src/system/input_handling.hpp
+++ src/system/input_handling.hpp
@@ -6,29 +6,29 @@
 #include "ECS_init.hpp"
 #include "global_entity_values.hpp"
 #include "game_actions.hpp"
-#include "input_handling_data.hpp"
+#include "engine_tools/input_handling_data.hpp"
 #endif
 
 #ifndef INPUT_HANDLING
 #define INPUT_HANDLING
 
 void input_to_action(std::string key, entt::registry &reg, entt::entity entity) {
-  game_actions = blank_actions;
+  reg.patch<actions>(entity,[&](auto &actions) {actions.actions = blank_actions;});
   switch(key_bind_to_enum[key]) {
     default: {
-      reg.get<actions>(entity).actions = blank_actions;
+      reg.patch<actions>(entity,[&](auto &actions) {actions.actions = blank_actions;});
     } break;
     case UP_MOVE: {
-      reg.get<actions>(entity).actions.y_move = 1;
+      reg.patch<actions>(entity,[&](auto &actions) {actions.actions.y_move = 1;});
     } break;
     case DOWN_MOVE: {
-      reg.get<actions>(entity).actions.y_move = -1;
+      reg.patch<actions>(entity,[&](auto &actions) {actions.actions.y_move = -1;});
     } break;
     case RIGHT_MOVE: {
-      reg.get<actions>(entity).actions.x_move = 1;
+      reg.patch<actions>(entity,[&](auto &actions) {actions.actions.x_move = 1;});
     } break;
     case LEFT_MOVE: {
-      reg.get<actions>(entity).actions.x_move = -1;
+      reg.patch<actions>(entity,[&](auto &actions) {actions.actions.x_move = -1;});
     } break;
   }
 }
