9f7ee1c9b6726852b634ef265bbe2502eff781e2
submariwastaken1time <79896755+submariwastaken1time@users.noreply.github.com>
2024-06-30 17:59:26 +0700

    functions to update the game added

---
+++ src/engine_tools/update_game.hpp
@@ -0,0 +1,34 @@
+#ifndef UPDATE_INCLUDES
+#define UPDATE_INCLUDES
+
+#include "ECS_init.hpp"
+#include "global_entity_values.hpp"
+#include "game_actions.hpp"
+#include "engine_tools/input_handling.hpp"
+#include "system/move.hpp"
+
+
+#endif
+
+#ifndef UPDATE_GAME
+#define UPDATE_GAME
+// update only the movement
+
+void update_monsters(entt::registry &reg) {
+  auto view_moves = reg.view<actions>();
+
+  for (auto entity : view_moves) {
+    move(entity, reg);
+  }
+}
+
+// update only the player based on inputs
+
+void update_player(entt::registry &reg, std::string key) {
+  auto view_player = reg.view<player_marker>();
+
+  for (auto player : view_player) {
+    input_to_action(key, reg, player);
+  }
+}
+#endif
