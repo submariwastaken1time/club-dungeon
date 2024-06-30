0881500ff8062986641575a52350f7f785ba8a74
submariwastaken1time <79896755+submariwastaken1time@users.noreply.github.com>
2024-06-30 21:33:37 +0700

    changed some values to use unsigned integers

--- src/entity/orc.hpp
+++ src/entity/orc.hpp
@@ -20,11 +20,11 @@ entt::entity create_orc(entt::registry &registry) {
   registry.emplace<name>(orc,"Orc");
   registry.emplace<pos>(orc, 0 , 0 );
   registry.emplace<icon>(orc, 'O');
-  registry.emplace<fg>(orc, 0 , 0 , 0 );
+  registry.emplace<fg>(orc, fg{255u , 255u , 255u} );
   registry.emplace<hp>(orc, 0 );
   registry.emplace<atk>(orc, 0 );
-  registry.emplace<xp>(orc, 0 );
-  registry.emplace<AI>(orc, 0 );
+  registry.emplace<xp>(orc, 0, 0 );
+  registry.emplace<AI>(orc, AI{0u} );
   registry.emplace<actions>(orc, blank_actions);
   return orc;
 }
--- src/entity/player.hpp
+++ src/entity/player.hpp
@@ -13,11 +13,11 @@ entt::entity create_player(entt::registry &registry) {
   registry.emplace<name>(player,"You");
   registry.emplace<pos>(player, 0 , 0 , 0);
   registry.emplace<icon>(player, '@');
-  registry.emplace<fg>(player, 255 , 255 , 255 );
+  registry.emplace<fg>(player, fg{255u , 255u , 255u} );
   registry.emplace<hp>(player, 0 );
   registry.emplace<atk>(player, 0 );
-  registry.emplace<xp>(player, 0 );
-  registry.emplace<AI>(player, 0 );
+  registry.emplace<xp>(player, 0, 0 );
+  registry.emplace<AI>(player, AI{0u} );
   registry.emplace<actions>(player, blank_actions);
   return player;
 }
