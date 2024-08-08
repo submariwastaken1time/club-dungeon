42e2ad95b9fd361bad1fc00d862bfcde74bda2aa
submariwastaken1time <79896755+submariwastaken1time@users.noreply.github.com>
2024-08-08 23:13:12 +0700

    new set nearby function, allowing for access of nearby entities without having to parse through the main registry

---
+++ src/system/modules/set_nearby.hpp
@@ -0,0 +1,17 @@
+// currently shelving this for now
+#ifndef SET_NEARBY_INCLUDES
+#define SET_NEARBY_INCLUDES
+
+#include "ECS_init.hpp"
+#include "engine_tools/set_nearby_tools.hpp"
+
+#endif
+
+#ifndef SET_NEARBY
+#define SET_NEARBY
+void set_nearby(entt::registry reg) {
+  auto any_ent_pos = reg.view<pos>();
+  for (entt:entity entity : any_ent_pos) {
+  }
+}
+#endif
