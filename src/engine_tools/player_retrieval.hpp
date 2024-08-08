c66d5cc5389b8fcb84fda3bf8fd949eca61bc686
submariwastaken1time <79896755+submariwastaken1time@users.noreply.github.com>
2024-08-08 23:04:36 +0700

    utility for retrieving the player

---
+++ src/engine_tools/player_retrieval.hpp
@@ -0,0 +1,16 @@
+#ifndef PLAYER_RETRIEVAL_INCLUDES
+#define PLAYER_RETRIEVAL_INCLUDES
+
+#include "ECS_init.hpp"
+
+#endif
+
+#ifndef PLAYER_RETRIEVAL
+#define PLAYER_RETRIEVAL
+
+auto player_view = reg.view<player_marker>()
+for (entt::entity entity : player_view) {
+  auto player = entity
+}
+
+#endif
