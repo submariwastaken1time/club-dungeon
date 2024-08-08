5abb2494d87af754b9500bb42ebdb172a72cb98c
submariwastaken1time <79896755+submariwastaken1time@users.noreply.github.com>
2024-08-08 23:06:36 +0700

    temporary function for changing the orc's location, allowing for a combat test

---
+++ src/system/modules/T_set_orc_pos.hpp
@@ -0,0 +1,21 @@
+// temp function to test combat
+#ifndef T_SET_ORC_POS_INCLUDES
+#define T_SET_ORC_POS_INCLUDES
+#include "ECS_init.hpp"
+#include "set_pos.hpp"
+#endif
+
+#ifndef T_SET_ORC_POS
+#define T_SET_ORC_POS
+void T_set_orc_pos(entt::registry reg) {
+
+auto orc_view = reg.view<name>()
+for (entt::entity entity : orc_view) {
+  if (reg.get<name>(entity) == "Orc") {
+  auto orc = entity;
+  }
+}
+
+set_pos(orc, reg, 5 , 5);
+}
+#endif
