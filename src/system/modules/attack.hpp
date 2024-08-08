0e1eee278c7951c189d657640582eb67c6da026d
submariwastaken1time <79896755+submariwastaken1time@users.noreply.github.com>
2024-08-08 23:07:42 +0700

    new function for attacking

---
+++ src/system/modules/attack.hpp
@@ -0,0 +1,21 @@
+#ifndef ATTACK_INCLUDES
+#define ATTACK_INCLUDES
+
+#include "ECS_init.hpp"
+
+#endif
+
+#idndef ATTACK
+#define ATTACK
+
+void attack(entt::entity attacker, entt::registry reg, entt::entity attacked) {
+  auto attack = reg.get(attacker)<atk>.atk;
+  auto bumped = reg.get(attacker)<bumped_tag>.bumped_tag;
+  auto atkd_has_HP = reg.all_of<hp>(attacked);
+
+  if (bumped && atkd_has_HP) {
+    reg.patch<HP>(attacked, [&](auto &HP) {HP.HP = HP.HP - attack;});
+  }
+}
+
+#endif
